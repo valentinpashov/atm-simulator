a05e13b6bed2fb70da70f9e0eda1bd57ecc6174d
Valentin Pashov <valentinpashov04@gmail.com>
2026-02-09 17:27:44 +0200

    Add Bill characteristics

--- src/Account.java
+++ src/Account.java
@@ -1,2 +1,33 @@
 public class Account {
+    private double balance;
+    private String pin;
+
+    public Account(double initialBalance, String initialPin) {
+        this.balance = initialBalance;
+        this.pin = initialPin;
+    }
+
+    public boolean checkPin(String inputPin) {
+        return this.pin.equals(inputPin);
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Успешно внесохте: " + amount + " лв.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Успешно изтеглихте: " + amount + " лв.");
+        } else {
+            System.out.println("Грешка: Недостатъчна наличност.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
